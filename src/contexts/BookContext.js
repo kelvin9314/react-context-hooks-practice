6371168813386aa083b0c788807a338a15ce0966
kelvin9314 <kelvin9314@gmail.com>
2020-01-21 08:34:04 +0800

    reating Context with Functional Components

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react';
+
+export const BookContext = createContext()
+
+const BookContextProvider = props => {
+  const [books, setBooks] = useState([
+    {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
+    {title: 'the final empire', author: 'brandon sanderson', id: 2},
+  ]);
+  const addBook = (title, author) => {
+    // setBooks([...books, {title, author, id: uuid()}]); 
+    console.log('hi');
+  };
+  const removeBook = (id) => {
+    setBooks(books.filter(book => book.id !== id));
+  }
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+}
+ 
+export default BookContextProvider;
